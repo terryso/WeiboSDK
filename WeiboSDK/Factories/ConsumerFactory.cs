ad76461e189c44188a56b9e8655de3acd253f5f4
terryso <suchuanyi@126.com>
2012-08-01 12:21:04 +0800

    去除真实的APP_KEY

--- WeiboSDK/Factories/ConsumerFactory.cs
+++ WeiboSDK/Factories/ConsumerFactory.cs
@@ -22,8 +22,8 @@ namespace WeiboSDK.Factories
                 case "ÐÂÀËÎ¢²©":
                     consumer = new Consumer
                     {
-                        Key = "59261381",
-                        Secret = "b8ead84f1d63e6518b6cb51d9885cb7b",
+                        Key = "ÄúµÄÐÂÀËÎ¢²©APP_KEY",
+                        Secret = "ÄúµÄÐÂÀËÎ¢²©APP_SECRET_KEY",
                         RequestTokenUri = "http://api.t.sina.com.cn/oauth/request_token",
                         AuthorizeUri = "http://api.t.sina.com.cn/oauth/authorize",
                         AccessTokenUri = "http://api.t.sina.com.cn/oauth/access_token"
@@ -32,8 +32,8 @@ namespace WeiboSDK.Factories
                 case "ÌÚÑ¶Î¢²©":
                     consumer = new Consumer
                     {
-                        Key = "b5853647954f4f1eaa2d647db7c828d7",
-                        Secret = "3d63a9b04c5c6d8793cca0e3b38eb9a2",
+                        Key = "ÄúµÄÌÚÑ¶Î¢²©APP_KEY",
+                        Secret = "ÄúµÄÌÚÑ¶Î¢²©APP_SECRET_KEY",
                         RequestTokenUri = "https://open.t.qq.com/cgi-bin/request_token",
                         AuthorizeUri = "https://open.t.qq.com/cgi-bin/authorize",
                         AccessTokenUri = "https://open.t.qq.com/cgi-bin/access_token",
